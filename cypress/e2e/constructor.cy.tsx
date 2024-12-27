1c631bdb891cae38e1e4923ea00bed218afc0c3f
Arthur-Adamov <artur.adamov03340@yandex.ru>
2024-12-27 18:54:39 +0300

    feat: Интеграционные тесты на Cypress написаны

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,82 @@
+describe('проверяем конструктор бургера', function() {
+  beforeEach(() => {
+    cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as('ingredients')
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user.json'}).as('user')
+    cy.window().then((win) => {
+      win.localStorage.setItem('refreshToken', 'mockRefreshToken')
+    })
+    cy.setCookie('accessToken', 'mockAccessToken')
+    cy.visit('http://localhost:4000');
+    cy.wait('@ingredients');
+    cy.log('Ингредиенты загружены');
+  })
+
+  afterEach(() => {
+    cy.window().then((win) => {
+      win.localStorage.removeItem('refreshToken')
+    })
+    cy.clearCookie('accessToken')
+  })
+  it('сервис должен быть доступен по адресу localhost:4000', function() {
+    cy.visit('http://localhost:4000');
+  })
+  it('тестируем создание заказа', () => {
+    cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as('order')
+
+    cy.get('[data-cy=ingredient-categories]')
+      .contains('Булки')
+      .next('ul')
+      .contains('Добавить')
+      .click()
+    cy.get('[data-cy=ingredient-categories]')
+      .contains('Начинки')
+      .next('ul')
+      .contains('Добавить')
+      .click()
+    cy.get('[data-cy=ingredient-categories]')
+      .contains('Соусы')
+      .next('ul')
+      .contains('Добавить')
+      .click()
+
+    cy.get('[data-cy=selected-bun]')
+      .contains('Краторная булка N-200i')
+      .should('exist')
+    cy.get('[data-cy=selected-main]')
+      .contains('Биокотлета из марсианской Магнолии')
+      .should('exist')
+    cy.get('[data-cy=selected-main]')
+      .contains('Соус Spicy-X')
+      .should('exist')
+
+    cy.get('[data-cy=create-order-block]')
+      .contains('Оформить заказ')
+      .click()
+    cy.get('[data-cy=order-details]')
+      .contains('64439')
+      .should('exist')
+      .should('be.visible')
+    
+    cy.get('[data-cy=modal-close-button]')
+      .click()
+    cy.get('[data-cy=order-details]')
+      .should('not.exist')
+
+    cy.get('[data-cy=selected-bun]')
+      .should('not.exist')
+    cy.get('[data-cy=selected-main]')
+      .should('not.exist')
+  })
+  it('тестируем работу модальных окон', () => {
+    cy.get('[data-cy=ingredient-categories]')
+      .contains('Краторная булка N-200i')
+      .click()
+    cy.get('[data-cy=modal-close-button]')
+      .click()  
+  })
+})
+
+
+
+
+
